845e4b54147f7c206fbcaa97a759a44599089286
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2026-08-27 22:58:09 -0500

    fix(main): New archive component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     loadComponent: () =>
-      import('./pages/home/home.component')
-        .then(m => m.HomeComponent)
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
 
   {
     path: 'about',
     loadComponent: () =>
-      import('./features/home/about/about.component')
-        .then(m => m.AboutComponent)
+      import('./features/home/about/about.component').then(
+        (m) => m.AboutComponent,
+      ),
+  },
+  {
+    path: 'archive',
+    loadComponent: () =>
+      import('./features/archive/archive.component').then(
+        (m) => m.ArchiveComponent,
+      ),
   },
 
   {
     path: '**',
-    redirectTo: ''
-  }
-
+    redirectTo: '',
+  },
 ];
---
+++ src/app/features/archive/archive.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface ArchiveCategory {
+  title: string;
+  description: string;
+  image: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-archive',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './archive.component.html',
+  styleUrl: './archive.component.scss'
+})
+export class ArchiveComponent {
+
+  categories: ArchiveCategory[] = [
+
+    {
+      title: 'Cómics',
+      description: 'Historias, personajes y aventuras que viven entre páginas.',
+      image: '../assets/img/comics.png',
+      route: '/archive/comics'
+    },
+
+    {
+      title: 'Videojuegos',
+      description: 'Los mundos digitales que nos acompañaron durante años.',
+      image: 'assets/images/archive/video-games.jpg',
+      route: '/archive/videojuegos'
+    },
+
+    {
+      title: 'Películas',
+      description: 'Héroes, villanos y universos que llegaron a la pantalla.',
+      image: 'assets/images/archive/movies.jpg',
+      route: '/archive/peliculas'
+    },
+
+    {
+      title: 'Series',
+      description: 'Historias que hicieron que esperáramos cada nuevo episodio.',
+      image: 'assets/images/archive/series.jpg',
+      route: '/archive/series'
+    },
+
+    {
+      title: 'Personajes',
+      description: 'Los protagonistas y antagonistas que forman nuestros universos favoritos.',
+      image: 'assets/images/archive/characters.jpg',
+      route: '/archive/personajes'
+    },
+
+    {
+      title: 'Retro',
+      description: 'Una mirada a aquello que nunca dejamos atrás.',
+      image: 'assets/images/archive/retro.jpg',
+      route: '/archive/retro'
+    }
+
+  ];
+
+}
